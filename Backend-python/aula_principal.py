a683abcce3676ea82e32b65d4225e10b94a3f428
Erivanderson Luiz <erivandersonfreitas29@gmail.com>
2025-07-30 12:42:25 -0300

    feito os desafio de fazer desconto para uma loja virtual

--- Backend-python/aula_principal.py
+++ Backend-python/aula_principal.py
@@ -489,3 +489,41 @@ saida = valor_salario - valor_imposto + valor_beneficio
 print(f"{saida:.2f}")
 
 
+# segundo desafio, Eplorando operadores e manipulação de strings 
+
+"""
+            Descrição
+
+Uma loja online deseja aplicar 
+descontos em seus produtos com base em cupons de desconto digitados 
+pelos clientes.
+
+             Regras de desconto:
+
+"DESCONTO10": 10% de desconto.
+"DESCONTO20": 20% de desconto.
+"SEM_DESCONTO": Sem desconto.
+
+             Entrada
+Preço original do produto.
+Código do cupom digitado.
+
+             Saída
+Preço final após aplicar o desconto. Com duas casas decimais.
+
+"""
+# Dicionário com os valores de desconto
+descontos = {
+    "DESCONTO10": 0.10,
+    "DESCONTO20": 0.20,
+    "SEM_DESCONTO": 0.00
+}
+
+# Entrada do usuário
+preco = float(input().strip())
+cupom = input().strip()
+
+# TODO: Aplique o desconto se o cupom for válido:
+
+
+    
